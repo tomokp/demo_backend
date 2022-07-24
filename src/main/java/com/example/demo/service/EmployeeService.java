18421af1f3314e69852e5ea9f80166e5581be38b
tomokp <tomoostkapelle@gmail.com>
2022-07-25 00:29:52 +0200

    initial

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/com/example/demo/serviceimpl/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return null;
+    }
+}
